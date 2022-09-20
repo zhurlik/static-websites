0d74fc746f928153d76caae5e5a6856e4e15112d
zhurlik <zhurlik@gmail.com>
2022-09-20 22:28:59 +0300

    how to import images in js files

--- src/desktop/entry/index.js
+++ src/desktop/entry/index.js
@@ -1,7 +1,9 @@
 'use strict';
+import testImg from '../image/index-img.png';
 
 window.addEventListener('load', () => {
     setTimeout(() => {
         document.write('<h1>Hello Desktop!!!</h1>');
+        document.write(`<img src="${testImg}" alt="desktop image"/>`);
     }, 2000);
 });
--- src/mobile/entry/index.js
+++ src/mobile/entry/index.js
@@ -1,7 +1,9 @@
 'use strict';
+import testImg from '../image/index-img.png';
 
 window.addEventListener('load', () => {
     setTimeout(() => {
         document.write('<h1>Hello Mobile!!!</h1>');
+        document.write(`<img src="${testImg}" alt="mobile image"/>`);
     }, 2000);
 });
