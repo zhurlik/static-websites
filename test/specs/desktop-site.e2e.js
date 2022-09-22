3da386fdac1f59e655ffe7676671825925b03f51
zhurlik <zhurlik@gmail.com>
2022-09-22 20:08:27 +0300

    first simple e2e test

---
+++ test/specs/desktop-site.e2e.js
@@ -0,0 +1,11 @@
+describe('Desktop web site', () => {
+
+    it('about.html page is loading', async () => {
+        await browser.url(`http://localhost:8081/about.html`);
+
+        await expect($('#about_image')).toBeExisting();
+        await expect($('#about_text')).toHaveTextContaining(
+            'Loading Desktop About page...');
+    });
+});
+
