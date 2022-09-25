c0e15cd8d774a33b8468449e40f00064c2c0eb5e
zhurlik <zhurlik@gmail.com>
2022-09-25 14:26:57 +0300

    add simple css files

--- src/desktop/entry/about.js
+++ src/desktop/entry/about.js
@@ -1,4 +1,5 @@
 'use strict';
+import '../template/page/about/css/about.css'
 
 window.addEventListener('load', () => {
    setTimeout(() => {
--- src/desktop/entry/index.js
+++ src/desktop/entry/index.js
@@ -1,9 +1,14 @@
 'use strict';
 import testImg from '../template/page/index/image/index-img.png';
+import '../template/page/index/css/index.css'
 
 window.addEventListener('load', () => {
     setTimeout(() => {
-        document.write('<h1>Hello Desktop!!!</h1>');
-        document.write(`<img src="${testImg}" alt="desktop image"/>`);
+        const testDiv = document.createElement("div");
+        testDiv.id = 'div_id';
+        testDiv.innerHTML = "<h1>Hello Desktop!!!</h1>";
+        testDiv.innerHTML +=  `<img width="150" height="150" src="${testImg}" alt="desktop image"/>`;
+        document.body.innerHTML = '';
+        document.body.appendChild(testDiv);
     }, 2000);
 });
